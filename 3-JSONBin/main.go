129a2017ed05747506344f3094412b24b08c4c24
levase <66877885+levase@users.noreply.github.com>
2025-04-10 17:46:10 +0400

    Update main.go
    remove test add bins

--- 3-JSONBin/main.go
+++ 3-JSONBin/main.go
@@ -35,26 +35,6 @@ func main() {
 		log.Printf("Failed to load bins: %v", err)
 	}
 
-	// Тестовое создание и добавление нового bin
-	newBin, err := bins.NewBin("1", "Test bin", false)
-	if err != nil {
-		log.Fatalf("Failed to create bin: %v", err)
-	}
-
-	if err := jsonStorage.AddBin(newBin); err != nil {
-		log.Fatalf("Failed to add bin: %v", err)
-	}
-
-	// Тестовое добавление второго bin
-	secondBin, err := bins.NewBin("2", "Test bin 2", true)
-	if err != nil {
-		log.Fatalf("Failed to create second bin: %v", err)
-	}
-
-	if err := jsonStorage.AddBin(secondBin); err != nil {
-		log.Fatalf("Failed to add second bin: %v", err)
-	}
-
 	fmt.Println("Current bins:")
 	for _, bin := range binList.Bins {
 		fmt.Printf("- %s (ID: %s, Created: %s, Private: %v)\n",
